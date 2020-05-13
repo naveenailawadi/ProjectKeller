67d5b258e45c84ca72a09bd43b352e3d20b60372
bensolomich <62868073+bensolomich@users.noreply.github.com>
2020-05-13 19:38:56 -0400

    changed tokens

--- Discord Bot/tokens.py
+++ Discord Bot/tokens.py
@@ -1,3 +1,3 @@
 # replace with your specific information token is bot token and user is owners username
-TOKEN = "NzA5NDU2ODA1NDYxMzYwNzEx.XrmNWA.g1PJlLocNU2ycWMJYWn4F3XXH4Y"
-OWNER = "bensolomich"
+TOKEN = "token"
+OWNER = "username"
