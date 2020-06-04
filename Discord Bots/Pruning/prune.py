2f9322364bf4c6c311f837277758d5e5e2cd84aa
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2020-06-04 15:45:16 +0000

    fixed spelling errors

--- Discord Bots/Pruning/prune.py
+++ Discord Bots/Pruning/prune.py
@@ -26,7 +26,7 @@ async def clean_server():
 
     # setting time objects
     current = datetime.datetime.now()
-    compareable = datetime.timedelta(days=DAYS)
+    comparable = datetime.timedelta(days=DAYS)
 
     while (client.is_closed):
         for guild in client.guilds:
@@ -35,16 +35,16 @@ async def clean_server():
             # checking each member individually
             for member in guild.members:
                 # checking if they joined recently
-                join_recently = ((current - member.joined_at) < compareable)
+                join_recently = ((current - member.joined_at) < comparable)
 
                 # checking if they recently sent messages
                 recent_message = False
                 for channel in guild.text_channels:
                     async for message in channel.history(limit=1000000):
-                        if((current - message.created_at < compareable) and (message.author == member)):
+                        if((current - message.created_at < comparable) and (message.author == member)):
                             recent_message = True
 
-                #kicks and counts if user didnt send a message recently and didnt join recently
+                # kicks and counts if user didnt send a message recently and didnt join recently
                 if(not(recent_message or join_recently)):
                     await guild.kick(member, reason=f"{DAYS} days of inactivity.")
                     members_kicked = members_kicked + 1
